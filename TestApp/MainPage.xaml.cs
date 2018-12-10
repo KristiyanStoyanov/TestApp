9a41aae17304e9912b0c74122c6f39b7e80fd9af
KristiyanStoyanov <christiyan.stoyanov@gmail.com>
2018-12-10 11:05:56 +0000

    Project created

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class MainPage : TabbedPage
+    {
+     
+        public MainPage()
+        {
+            this.Children.Add(new About());
+            this.Children.Add(new Settings());        
+            InitializeComponent();
+        }
+
+       private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            IsBusy = true;
+            await Navigation.PushAsync(new MenuPage());
+        }
+    }
+}
